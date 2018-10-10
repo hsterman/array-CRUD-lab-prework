5074a523cb6745c150236ad7a42eebc589e5e6cf
heather sterman <heather.sterman531@gmail.com>
2018-10-10 17:08:25 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,8 +33,7 @@ end
 
 def retrieve_first_element_from_array(array)
   famous_cats = ["lil bub", "grumpy cat", "maru", "pusheen"]
-  
-  
+  famous_cats.first
 end
 
 def retrieve_last_element_from_array(array)
